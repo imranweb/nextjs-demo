b75aa5c81763ac003d344c3730f567f947a5fa45
imranweb <imranweb97@gmail.com>
2024-01-18 16:35:10 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+// next.config.mjs
+import { withHighlightConfig } from '@highlight-run/next/config'
+
+const nextConfig = {
+	experimental: {
+		// instrumentationHook: true,
+	},
+	productionBrowserSourceMaps: true,
+	images: { domains: ['i.travelapi.com'] },
+}
+
+export default withHighlightConfig(nextConfig)
